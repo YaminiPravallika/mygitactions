55cad5aa184bda3a12ccd3501d7beffe4996624a
YaminiPravallika <88648008+YaminiPravallika@users.noreply.github.com>
2022-01-18 20:06:08 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
-class Simple{
-    public static void main(String args[]){
-            System.out.println("hello javatpoint");
-        }
+public class HelloWorld {
+  public static void main(String...args) {
+    System.out.println("Hello, world!");
+  }
 }
